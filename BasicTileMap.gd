40181e6c6aa4b2647ca18d6c222224b328a13312
Chris Pentoney <huvetahw@gmail.com>
2023-09-15 15:52:32 -0700

    Adding basic procedural gen for map

---
+++ BasicTileMap.gd
@@ -0,0 +1,36 @@
+extends TileMap
+
+var ground_tile = preload("res://images/ground1.png")
+var wall_tile = preload("res://images/wall1.png")
+var obstacle_tile = preload("res://images/obs1.png")
+
+func _ready():
+	generate_level()
+
+func generate_level():
+	# Define the size of the level (adjust as needed)
+	var level_width = 100
+	var level_height = 50
+
+	# Clear existing tiles
+	clear()
+
+	# Generate ground
+	for x in range(level_width):
+		for y in range(level_height):
+			set_cell(0, Vector2i(x, y), 0, Vector2i(3, 7))
+
+	# Generate walls (top and sides)
+	for x in range(level_width):
+		set_cell(0, Vector2i(x, 0), 0, Vector2i(1, 1))
+		set_cell(0, Vector2i(x, level_height - 1), 0, Vector2i(1, 1))
+
+	for y in range(level_height):
+		set_cell(0, Vector2i(0, y), 0, Vector2i(11, 12))
+		set_cell(0, Vector2i(level_width - 1, y), 0, Vector2i(11, 12))
+
+	# Generate random obstacles
+	for x in range(level_width * level_height*2):  # Adjust density as needed
+		var obstacle_x = randi() % (level_width - 2) + 1
+		var obstacle_y = randi() % (level_height - 2) + 1
+		set_cell(0, Vector2i(obstacle_x, obstacle_y), 1)
--- StartMenu.gd
+++ StartMenu.gd
@@ -1,19 +1,14 @@
 extends Node
 
-# Load the high scores and game scenes
-var high_scores_scene = preload("res://HighScores.tscn")
-var game_scene = preload("res://Game.tscn")
 
 func _ready():
 	# Connect button signals
 	pass
-	#$ButtonHighScores.pressed.connect(_on_high_scores_pressed)
 
-func _on_new_game_pressed():
-	# Switch to the Game scene
-	get_tree().change_scene_to_file(game_scene)
 
 func _on_high_scores_pressed():
 	# Switch to the High Scores scene
 	get_tree().change_scene_to_file("res://HighScores.tscn")
 
+func _on_button_new_game_pressed():
+	get_tree().change_scene_to_file("res://Game.tscn")
