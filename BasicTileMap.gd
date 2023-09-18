277242f70e4493cde16f4f5fb6b2f64e46bd7945
Chris Pentoney <huvetahw@gmail.com>
2023-09-18 13:03:15 -0700

    basic collision

--- BasicTileMap.gd
+++ BasicTileMap.gd
@@ -4,6 +4,9 @@ var ground_tile = preload("res://images/ground1.png")
 var wall_tile = preload("res://images/wall1.png")
 var obstacle_tile = preload("res://images/obs1.png")
 
+var collision_shape: CollisionShape2D
+
+
 func _ready():
 	generate_level()
 
@@ -20,7 +23,7 @@ func generate_level():
 		for y in range(level_height):
 			set_cell(0, Vector2i(x, y), 0, Vector2i(3, 7))
 
-	# Generate walls (top and sides)
+	# Generate walls (top and sides)ff
 	for x in range(level_width):
 		set_cell(0, Vector2i(x, 0), 0, Vector2i(1, 1))
 		set_cell(0, Vector2i(x, level_height - 1), 0, Vector2i(1, 1))
@@ -33,4 +36,4 @@ func generate_level():
 	for x in range(level_width * level_height*2):  # Adjust density as needed
 		var obstacle_x = randi() % (level_width - 2) + 1
 		var obstacle_y = randi() % (level_height - 2) + 1
-		set_cell(0, Vector2i(obstacle_x, obstacle_y), 1)
+		set_cell(0, Vector2i(obstacle_x, obstacle_y), 1, Vector2i(12, 6))
---
+++ PlayerCharacterBody2D.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+
+var collision_shape: CollisionShape2D
+
+@onready var _animation_player = $AnimationPlayer
+
+	
+func _process(_delta):
+	if Input.is_action_pressed("ui_right"):
+		_animation_player.play("run")
+	elif Input.is_action_pressed("ui_left"):
+		_animation_player.play("run")
+	elif Input.is_action_pressed("ui_up"):
+		_animation_player.play("run")
+	elif Input.is_action_pressed("ui_down"):
+		_animation_player.play("run")
+	else:
+		_animation_player.stop()
+
+	
+func _physics_process(delta):
+	
+	# Get the player's input.
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	# Normalize the input vector. This ensures that the character moves at the same speed in all directions.
+	input = input.normalized()
+
+	# Set the character's velocity based on the input.
+	velocity = input * SPEED
+	
+	# Move the character.
+	var collision = move_and_collide(velocity * delta)
+	#if collision:
+	#	print("I collided with ", collision.get_collider().name)
